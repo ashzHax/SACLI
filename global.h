bca91cf9bdc3f3bac439537c4d88ffb93eaa2b97
ashzHax <ashkim2001@gmail.com>
2023-08-13 13:35:12 +0900

    Temporary commit, for code copy (yes, I am very sad about this too)

---
+++ global.h
@@ -0,0 +1,7 @@
+#ifndef __GLOBAL_H__
+#define __GLOBAL_H__
+
+#define DEBUG_MODE 1
+typedef unsigned char BOOL;
+
+#endif // __GLOBAL_H__
--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #define EXIT_ARGUMENT_PARSE_FAIL	-2
 #define EXIT_INVALID_ARGUMENT		-3
 
+// modes
 enum {
 	ADD,
 	REMOVE,
@@ -23,6 +24,26 @@ enum {
 	REVERT,
 	ROLLBACK
 };
+
+// move to lib :/
+inline BOOL cpy_string(char *s, char *d, size_t t)
+{
+	int i;
+
+	if(s == NULL || d == NULL || t <= 0) {
+		return 0;
+	}
+
+	for(i=0; i<t; i++) {
+		if( s[i] == '\0' ) {
+			break;
+		}
+		d[i] = s[i];
+	}
+	d[i] = '\0';
+
+	return 1;
+}
 
 inline void convert_str_lower(char *s, size_t t)
 {
@@ -39,11 +60,75 @@ inline void convert_str_lower(char *s, size_t t)
 	}
 }
 
+/* 
+ * handmade strlen() 
+ *
+ * note
+ * Personally, I think it is a very bad idea to modify the function arguments.
+ * But for fast processing, declaring a "could be saved" variable is a waste of time.
+ * 
+ * return
+ * length of "s" (till character NULL)
+ * -1 on error
+*/
+size_t get_str_len(char *s)
+{
+	size_t len = 0;
+
+	if(s == NULL) {
+		return -1;
+	}
+
+	while(*(s++) != '\0') {
+		len++;
+	}
+	return len;
+}
+
+// handmade strncmp, but a full compare of length.
+// thinking about it, its more like Java isEqual()
+unsigned char chk_str_diff(char *a, char *b)
+{
+	int i;
+	int len;
+
+	if(a == NULL || b == NULL) {
+		return (unsigned char)-1;
+	}
+
+	len = get_str_len(a);
+
+	if(len != get_str_len(b)) {
+		return (unsigned char)-1;
+	}
+
+	for(i=0; i<(int)len; i++) {
+		if(a[i] != b[i]) {
+			return (unsigned char)len-i;
+		}
+	}
+
+	return (unsigned char)0;
+}
+
 int opt_parse(int argc, char** argv)
 {
+	int len;
+
+#if DEBUG_MODE // print argument list
 	int i = 0;
+
 	for(i = 0; i < argc; i++) {
 		printf("[%d] - argv[%s]\n", i, argv[i]);
+	}
+#endif // DEBUG_MODE
+
+	len = get_str_len(argv[1]);
+
+	printf("---> [%s():%d] ---> len:%d\n", __func__, __LINE__, len);
+
+	if (chk_str_diff(argv[1], "add") == 0) { // if same
+		printf("found ADD!!!!\n");
 	}
 
 	return 0;
